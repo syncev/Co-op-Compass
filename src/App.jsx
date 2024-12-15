436adc58dcf305ddf397d5dae772c4a0a7e2bd82
Santiago Hernandez <santuche90@gmail.com>
2024-12-15 10:07:16 -0300

    add pc res categories

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,7 @@ function App() {
   const genericListRef = useRef(null);
   const filterPicker = useRef(null);
   const platformsRef = useRef(null);
-  const playersRef = useRef(null);
   const genresRef = useRef(null);
-  
 
   function searchInputChange(e) {
     setSearchContent(e.target.value);
@@ -40,7 +38,7 @@ function App() {
   }
   function genericListBackHandle() {
     homePage = true;
-    
+
     showHome();
   }
 
@@ -73,7 +71,7 @@ function App() {
       searchContent !== " "
     ) {
       oldListRemover();
-      getGamesList(40, ".genericList", searchQueryContent,undefined,"");
+      getGamesList(40, ".genericList", searchQueryContent, undefined, "");
       currentSearchQuery = searchContent;
       showList();
     }
@@ -125,74 +123,91 @@ function App() {
 
   function toggleFilter() {
     filterPicker.current.classList.toggle("hidden");
-    
   }
-  function filterFill(){
+  function filterFill() {
     const formGenres = document.getElementById("genresForm");
     gameGenresList.current.forEach((category) => {
       const label = document.createElement("label");
-      const input =document.createElement("input");
+      const input = document.createElement("input");
       input.setAttribute("type", "checkbox");
       input.setAttribute("name", category.name);
       input.setAttribute("id", category.id);
       label.appendChild(input);
       label.appendChild(document.createTextNode(category.name));
       formGenres.appendChild(label);
-    })
+    });
     const formPlatforms = document.getElementById("platformsForm");
     gamePlatformsList.current.forEach((category) => {
       const label = document.createElement("label");
-      const input =document.createElement("input");
+      const input = document.createElement("input");
       input.setAttribute("type", "checkbox");
       input.setAttribute("name", category.name);
       input.setAttribute("id", category.id);
       label.appendChild(input);
       label.appendChild(document.createTextNode(category.name));
       formPlatforms.appendChild(label);
-    })
-  
+    });
   }
-function resetFilter(){
-  genresRef.current.reset()
-  platformsRef.current.reset()
-
-}
+  function resetFilter() {
+    genresRef.current.reset();
+    platformsRef.current.reset();
+  }
   function confirmFilter() {
-    const genresArray = Array.from(genresRef.current).filter((input) => input.checked);
-    var genresIdArray = ""
+    const genresArray = Array.from(genresRef.current).filter(
+      (input) => input.checked
+    );
+    var genresIdArray = "";
     const genresNameArray = genresArray.map((genre) => genre.name).join(", ");
     if (genresArray.length > 0) {
-      genresIdArray = `&genres=${genresArray.map((genre) => genre.id).join(",").toLowerCase()}`
+      genresIdArray = `&genres=${genresArray
+        .map((genre) => genre.id)
+        .join(",")
+        .toLowerCase()}`;
     }
 
-      const platformsArray = Array.from(platformsRef.current).filter((input) => input.checked)
-      var platformsIdArray = "";
-      const platformsNameArray = platformsArray.map((platform) => platform.name).join(", ");
-      if (platformsArray.length > 0) {
-        platformsIdArray = `&platforms=${platformsArray.map((platform) => platform.id).join(",").toLowerCase()}`
-      }
+    const platformsArray = Array.from(platformsRef.current).filter(
+      (input) => input.checked
+    );
+    var platformsIdArray = "";
+    const platformsNameArray = platformsArray
+      .map((platform) => platform.name)
+      .join(", ");
+    if (platformsArray.length > 0) {
+      platformsIdArray = `&platforms=${platformsArray
+        .map((platform) => platform.id)
+        .join(",")
+        .toLowerCase()}`;
+    }
 
     toggleFilter();
     oldListRemover();
 
-    getGamesList(50, ".genericList", genresIdArray, genresNameArray, "", platformsIdArray, platformsNameArray );
+    getGamesList(
+      50,
+      ".genericList",
+      genresIdArray,
+      genresNameArray,
+      "",
+      platformsIdArray,
+      platformsNameArray
+    );
   }
   async function getGamesList(
     page_size = 20,
     querySelector = ".topRated-list",
     searcPrmtr = "",
     categoryTitle = "",
-    dates="&dates=2015-01-01,2022-12-31",
+    dates = "&dates=2015-01-01,2022-12-31",
     platformsIdArray = "",
-    platformsTitle = "",
+    platformsTitle = ""
   ) {
     try {
       const res = await fetch(
         `https://api.rawg.io/api/games?key=${LLAVE}&tags=co-op,multiplayer&ordering=-metacritic&page_size=${page_size}${searcPrmtr}${dates}${platformsIdArray}`
       );
-        if(!res.ok){
-          throw new Error(`API request failed with status: ${res.status}`);
-        }
+      if (!res.ok) {
+        throw new Error(`API request failed with status: ${res.status}`);
+      }
       const data = await res.json();
       const gamesListData = data.results;
       const querySelectedElement = document.querySelector(`${querySelector}`);
@@ -381,11 +396,10 @@ function resetFilter(){
     gameCategoriesList.slice(0, 8).forEach((category) => {
       const categoriesLI = document.createElement("li");
       categoriesLI.innerHTML = category.name;
-      categoriesLI.onclick = function (){
+      categoriesLI.onclick = function () {
         categoriesHandler(category.id, category.name, listID);
-      } ;
+      };
 
-      
       categoriesContainer.appendChild(categoriesLI);
     });
   }
@@ -404,7 +418,8 @@ function resetFilter(){
       gameCategoriesList.slice(8).forEach((category) => {
         const categoriesLI = document.createElement("li");
         categoriesLI.innerHTML = category.name;
-        categoriesLI.onclick = () =>categoriesHandler(category.id, category.name, listID);
+        categoriesLI.onclick = () =>
+          categoriesHandler(category.id, category.name, listID);
         categoriesContainer.appendChild(categoriesLI);
       });
     }
@@ -416,17 +431,16 @@ function resetFilter(){
   }
   function categoriesHandler(categoryId, categoryName, listID) {
     homePage = false;
-        oldListRemover();
-        showList();
-        getGamesList(
-          100,
-          ".genericList",
-          `&${listID}=` + categoryId,
-          categoryName,
-          "",
-        );
+    oldListRemover();
+    showList();
+    getGamesList(
+      100,
+      ".genericList",
+      `&${listID}=` + categoryId,
+      categoryName,
+      ""
+    );
   }
-  
 
   return (
     <div className="appContainer">
@@ -495,21 +509,11 @@ function resetFilter(){
             <div className="filterPicker-categories">
               <div className="filterCategories-wrapper">
                 <h3>Categories</h3>
-                <form action="" ref={genresRef }id="genresForm">
-                  
-                </form>
+                <form action="" ref={genresRef} id="genresForm"></form>
               </div>
               <div className="filterConsoles-wrapper">
                 <h3>Platforms</h3>
-                <form action="" ref={platformsRef} id="platformsForm">
-                 
-                </form>
-              </div>
-              <div className="filterPlayers-wrapper" id="playersForm">
-                <h3>Players</h3>
-                <form action="" ref={playersRef}>
-                  
-                </form>
+                <form action="" ref={platformsRef} id="platformsForm"></form>
               </div>
             </div>
           </div>
@@ -519,20 +523,24 @@ function resetFilter(){
         <div className="genericList" ref={genericListRef}></div>
       </section>
       <section className="categories " ref={categoriesRef}>
-        <div className="categories-header">
-          <h2 className="secondaryHeader">Genre</h2>
+        <div className="categories-wrapper">
+          <div className="categories-header">
+            <h2 className="secondaryHeader">Genre</h2>
+          </div>
+          <ul className="categories-list " id="genres"></ul>
+          <button className="more-btn" id="moreGenres" onClick={moreLoader}>
+            More
+          </button>
         </div>
-        <ul className="categories-list " id="genres"></ul>
-        <button className="more-btn" id="moreGenres" onClick={moreLoader}>
-          More
-        </button>
-        <div className="categories-header">
-          <h2 className="secondaryHeader">Platforms</h2>
+        <div className="categories-wrapper">
+          <div className="categories-header">
+            <h2 className="secondaryHeader">Platforms</h2>
+          </div>
+          <ul className="categories-list " id="platforms"></ul>
+          <button className="more-btn" id="morePlatforms" onClick={moreLoader}>
+            More
+          </button>
         </div>
-        <ul className="categories-list " id="platforms"></ul>
-        <button className="more-btn" id="morePlatforms" onClick={moreLoader}>
-          More
-        </button>
       </section>
       <section
         className="gameDescription hidden"
